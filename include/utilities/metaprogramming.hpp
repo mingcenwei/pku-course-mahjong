2fb23f8041d2606ae5c3b776e8b1675bdbaff017
sAy <47605337+mingcenwei@users.noreply.github.com>
2020-04-11 16:35:50 +0800

    Add TemplateAssert0_t and AssertIntegral_t helpers

--- include/utilities/metaprogramming.hpp
+++ include/utilities/metaprogramming.hpp
@@ -10,14 +10,27 @@ namespace utilities
     template <typename AnyType>
     inline constexpr bool falseForStaticAssert_v {false};
 
-    // Enable "TypeToEnable" if and only all "Assertions" are true. Useful for
+    // Enable "TypeToEnable" if and only all "assertions" are true. Useful for
     // SFINAE.
-    template <typename TypeToEnable = bool, bool... Assertions>
-    using TemplateAssert_t = std::enable_if<(Assertions && ...), TypeToEnable>;
+    template <typename TypeToEnable = bool, bool... assertions>
+    using TemplateAssert_t = std::enable_if<(assertions && ...), TypeToEnable>;
+    // Convenient form of "TemplateAssert_t" which lets "TypeToEnable" be bool.
+    template <bool... assertions>
+    using TemplateAssert0_t = TemplateAssert_t<bool, assertions...>;
 
+    // Enabled if and only if "Integral" is a integral type.
+    template <typename Integral>
+    using AssertIntegral_t =
+        TemplateAssert_t<bool, std::is_integral_v<Integral>>;
+
+    //Add lvalue reference to T if condition is true.
+    template <typename T, bool condition>
+    using LvalueRefIf_t = std::
+        conditional_t<condition, std::add_lvalue_reference_t<T>, T>;
+
+    // Add lvalue reference to T if T is not of a scalar type.
     template <typename T>
-    using RefIfNotScalar_t = std::
-        conditional_t<std::is_scalar_v<T>, T, std::add_lvalue_reference_t<T>>;
+    using LvalueRefIfNotScalar_t = LvalueRefIf_t<T, !std::is_scalar_v<T>>;
 } // namespace utilities
 
 #endif
