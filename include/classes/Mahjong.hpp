29c148fb35b8162a6e41a9307d7371634904efde
sAy <47605337+mingcenwei@users.noreply.github.com>
2020-04-11 15:10:06 +0800

    Add some basic Mahjong traits

--- include/classes/Mahjong.hpp
+++ include/classes/Mahjong.hpp
@@ -1,16 +1,21 @@
 #ifndef MAHJONG_HPP_20200409_
 #define MAHJONG_HPP_20200409_
 
-#include <cstddef>
-
 namespace name::private_detail
-{
-
-}
+{}
 
 namespace name
 {
-    struct MahjongTraits {};
-}
+    template <typename Mahjong_>
+    struct MahjongTraits
+    {
+        static constexpr bool isTong(Mahjong_ const&) noexcept;
+        static constexpr bool isTiao(Mahjong_ const&) noexcept;
+        static constexpr bool isWan(Mahjong_ const&) noexcept;
+        static constexpr bool isFeng(Mahjong_ const&) noexcept;
+        static constexpr bool isJian(Mahjong_ const&) noexcept;
+        static constexpr bool isHua(Mahjong_ const&) noexcept;
+    };
+} // namespace name
 
 #endif
